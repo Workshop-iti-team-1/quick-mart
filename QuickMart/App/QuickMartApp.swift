494037d435d606f7d20b5f015200015dbb5c769b
ibrahimsiam25 <ebrahimfathy2822@gmail.com>
2026-06-28 22:38:30 +0300

    refactor: integrate Firebase via AppDelegate and clean up project file references

--- QuickMart/App/QuickMartApp.swift
+++ QuickMart/App/QuickMartApp.swift
@@ -8,10 +8,18 @@
 import SwiftUI
 import Firebase
 
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
+
 @main
 struct QuickMartApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     @State private var router = AppRouter()
-    
     var body: some Scene {
         WindowGroup {
             RootView()
