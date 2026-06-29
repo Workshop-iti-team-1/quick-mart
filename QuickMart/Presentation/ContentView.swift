7b6d4739c4ff792eab9b41352ad33023868e19b7
ibrahimsiam25 <ebrahimfathy2822@gmail.com>
2026-06-29 20:29:00 +0300

    ٠٠

--- QuickMart/Presentation/ContentView.swift
+++ QuickMart/Presentation/ContentView.swift
@@ -82,4 +82,8 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
     }
+
+
+
+    
 }
