7d5e995820e00d1c5885eabf9d2242ba872809df
ibrahimsiam25 <ebrahimfathy2822@gmail.com>
2026-07-04 17:16:42 +0300

    refactor: replace MockProfileView with ProfileView and update navigation routes

--- QuickMart/Modules/Profile/Presentation/View/ProfileView.swift
+++ QuickMart/Modules/Profile/Presentation/View/ProfileView.swift
@@ -72,15 +72,7 @@ struct ProfileView: View {
          .clipShape(RoundedCorner(radius: 30, corners: [.topLeft, .topRight]))
      }}
  */
-struct RoundedCorner: Shape {
-    var radius: CGFloat = .infinity
-    var corners: UIRectCorner = .allCorners
 
-    func path(in rect: CGRect) -> Path {
-        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
-        return Path(path.cgPath)
-    }
-}
 
 // MARK: - Preview
 struct ProfileScreen_Previews: PreviewProvider {
--- QuickMart/Modules/Profile/Presentation/ViewModel/ProfileViewModel.swift
+++ QuickMart/Modules/Profile/Presentation/ViewModel/ProfileViewModel.swift
@@ -20,7 +20,7 @@ class ProfileViewModel : ObservableObject{
     
     var personalItems: [MenuItem] {
         [
-            MenuItem(icon: "box", title: AppStrings.Profile.shippingAddress, trailing: .chevron(route: .shippingAddress)),
+            MenuItem(icon: "box", title: AppStrings.Profile.shippingAddress, trailing: .chevron(route: .shippingAddresses)),
             MenuItem(icon: "card-tick", title: AppStrings.Profile.paymentMethod, trailing: .chevron(route: .paymentMethod)),
             MenuItem(icon: "receipt-edit", title: AppStrings.Profile.orderHistory, trailing: .chevron(route: .orderHistory))
         ]
