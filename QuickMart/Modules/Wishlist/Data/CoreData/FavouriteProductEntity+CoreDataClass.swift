d85b3a20a04a28ff6c3432c187db5fee155aab51
alaaayman77 <alaaaymanfouad@gmail.com>
2026-07-04 02:43:08 -0700

    implemented core data stack and entity properties

---
+++ QuickMart/Modules/Wishlist/Data/CoreData/FavouriteProductEntity+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  FavouriteProductEntity+CoreDataClass.swift
+//  QuickMart
+//
+//  Created by Alaa Ayman on 04/07/2026.
+//
+
+
+// Data/CoreData/FavoriteProductEntity+CoreDataClass.swift
+import Foundation
+import CoreData
+
+@objc(FavouriteProductEntity)
+public class FavouriteProductEntity: NSManagedObject {}
+
